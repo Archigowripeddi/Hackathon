f7b8618a2ebaa74ab6356438a1c1dab5b39c7023
archi <archithagowripeddi04@gmail.com>
2024-10-05 14:20:59 +0530

    Final Commit

---
+++ app/src/main/java/com/example/codeathon/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.codeathon;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText username = findViewById(R.id.username);
+        MaterialButton regbtn = findViewById(R.id.signupbtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username1 = username.getText().toString();
+
+                // Display the username in a Toast message
+                Toast.makeText(MainActivity.this, "Username is: " + username1, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
